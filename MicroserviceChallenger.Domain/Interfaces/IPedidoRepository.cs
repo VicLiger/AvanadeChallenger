d1d95e1ff4df5e92d582f129b0eeef8fcb961e52
Victor Liger <victor.liger0800@gmail.com>
2025-08-21 20:03:51 -0300

    🚧 adicionando interfaces de repository no dominio e criando projeto da infra

---
+++ MicroserviceChallenger.Domain/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,17 @@
+﻿using MicroserviceChallenger.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroserviceChallenger.Domain.Interfaces
+{
+    public interface IPedidoRepository
+    {
+        Task AdicionarAsync(Pedido pedido);
+        Task AtualizarAsync(Pedido pedido);
+        Task<Pedido?> ObterPorIdAsync(Guid pedidoId);
+        Task<IEnumerable<Pedido>> ObterTodosAsync();
+    }
+}
