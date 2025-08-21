fcfa6d5f9b6ee4f22059066ff8819782007f442d
Victor Liger <victor.liger0800@gmail.com>
2025-08-21 19:53:22 -0300

    🚧 criando interface de produto e suas funcionalidades base

---
+++ MicroserviceChallenger.Application/Interfaces/IProdutoService.cs
@@ -0,0 +1,19 @@
+﻿using MicroserviceChallenger.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroserviceChallenger.Application.Interfaces
+{
+    public interface IProdutoService
+    {
+        Task AdicionarProduto(Produto produto);
+        Task<IEnumerable<string>> ConsultarCatalogoProdutosQuantidade();
+        Task AtualizarEstoque(Produto produto, int quantidadeComprada);
+
+
+
+    }
+}
